c4b32827443364051b3e54604ed2c2201549665f
nkristoporus <nkristoporus@gmail.com>
2019-04-18 10:26:11 +0800

    Fix Merging

--- src/app/evaluate-experience/evaluate-experience.page.ts
+++ src/app/evaluate-experience/evaluate-experience.page.ts
@@ -1,10 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Router, ActivatedRoute} from '@angular/router';
+
 import { EvaluationService } from '../evaluation.service';
-import { Location } from '@angular/common';
-import { SessionService } from '../session.service';
-
-
 
 @Component({
   selector: 'app-evaluate-experience',
@@ -14,29 +11,21 @@ import { SessionService } from '../session.service';
 export class EvaluateExperiencePage implements OnInit {
 
   bookingId: number;
-  userId: number;
-  remark: String;
   score: number;
+  remark: string;
 
-  constructor(private router: Router, private activatedRoute: ActivatedRoute, private evaluationService:EvaluationService, private location:Location, private sessionService: SessionService) {
-    this.bookingId = parseInt(this.activatedRoute.snapshot.paramMap.get('bookingId'));
-    this.userId = this.sessionService.getCurrentUser().userId;
-   }
+  constructor(private router: Router, private activatedRoute: ActivatedRoute, private evaluationService:EvaluationService) { }
 
   ngOnInit() {
+    this.bookingId = parseInt(this.activatedRoute.snapshot.paramMap.get('id'));
   }
 
-  evaluate(){
-    this.evaluationService.createEvaluationFromGuest(this.score,this.remark,this.bookingId,this.userId).subscribe(
-      response=>{console.log("Creation success"); this.location.back();},
-      error=>{console.log("error in evaluation");}
-    )
-  }
-  
   submit() {
     console.log("ts: "+this.bookingId);
     this.evaluationService.createEvaluateExperience(this.score, this.remark, this.bookingId).subscribe(
       response=>{this.router.navigate(['view-booking'])},
       error=>{console.log(error)}
+    )
+  }
 
 }
